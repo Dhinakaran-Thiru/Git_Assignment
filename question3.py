c6b9166c33d89272d8689b7c3d0b9c640ed0ac45
Dhinakaran <dhina15003@gmail.com>
2024-03-04 12:48:24 +0530

    make some changes in file and then commited

---
+++ question3.py
@@ -0,0 +1,10 @@
+def is_perfect_square(n):
+    # If the square root of the number is an integer, it's a perfect square
+    return int(n ** 0.5) ** 2 == n
+
+# Example usage:
+num1 = 25
+num2 = 30
+
+print(is_perfect_square(num1))  # Output: True (25 is a perfect square)
+print(is_perfect_square(num2))  # Output: False (30 is not a perfect square)
